d0c0003782dfcf856140f15ad7f9ababa18997f1
andrewariley87 <andrewariley87@gmail.com>
2015-12-28 15:31:26 -0500

    removes comment in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,3 @@ var ReactDom = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
 ReactDom.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
-//test
